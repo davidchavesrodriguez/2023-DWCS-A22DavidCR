33e7388ed6b19c6adae527593938e846fe765dc6
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-09-26 12:14:12 +0200

    10 and 11 updated

--- task3_10.php
+++ task3_10.php
@@ -2,96 +2,113 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
 
 <body>
   <style>
-    .role {
-      display: block;
+    #required {
+      color: red;
+      font-size: 10px;
     }
   </style>
 
   <h1>Novell Services Login</h1>
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <label for="username">Username: </label>
-    <input type="text" name="username" /><br />
-    <label for="password">Password: </label>
-    <input type="password" name="password" />
+    <label for="username">Username:
+    </label>
+    <input type="text" name="username" required value="
+                <?php echo isset($_POST['username']) ? htmlspecialchars($_POST['username']) : ''; ?>" />
+    <br>
+    <label for="username" id="required">*Required field*</label>
+    <br>
+    <label for="password">Password:
+    </label>
+    <input type="password" name="password" required value="
+                <?php echo isset($_POST['password']) ? htmlspecialchars($_POST['password']) : ''; ?>" />
+    <br>
+    <label for="username" id="required">*Required field*</label>
+    <br>
+    <label for="city">City of employment:
+    </label>
+    <input type="text" name="city" value="
+                <?php echo isset($_POST['city']) ? htmlspecialchars($_POST['city']) : ''; ?>" />
     <br />
-
-    <label for="city">City of employment: </label>
-    <input type="text" name="city" /><br />
     <label for="server">Web server</label>
     <select name="server" id="server">
-      <option value="northAmerica">North America</option>
-      <option value="europe">Europe</option>
-      <option value="asia">Asia</option>
-      <option value="southAmerica">South America</option>
+      <option value="northAmerica" <?php if (
+                                      isset($_POST['server'])
+                                      && $_POST['server'] == 'northAmerica'
+                                    )
+                                      echo 'selected'; ?>>
+        North America</option>
+      <option value="europe" <?php if (
+                                isset($_POST['server'])
+                                && $_POST['server'] == 'europe'
+                              )
+                                echo 'selected'; ?>>
+        Europe</option>
+      <option value="asia" <?php if (
+                              isset($_POST['server'])
+                              && $_POST['server'] == 'asia'
+                            )
+                              echo 'selected'; ?>>
+        Asia</option>
+      <option value="southAmerica" <?php if (
+                                      isset($_POST['server'])
+                                      && $_POST['server'] == 'southAmerica'
+                                    )
+                                      echo 'selected'; ?>>
+        South America</option>
     </select>
     <br />
     <br />
 
-    <label for="role">Please, specify your role: </label> <br />
+    <label for="role">Please, specify your role:
+    </label>
+    <br />
     <label for="admin">Admin</label>
-    <input type="radio" name="role" id="admin" value="admin" /> <br />
+    <input type="radio" name="role" id="admin" value="admin" <?php if (isset($_POST['role']) && $_POST['role'] == 'admin')
+                                                                echo 'checked'; ?> />
+    <br />
     <label for="engineer">Engineer</label>
-    <input type="radio" name="role" id="engineer" value="engineer" /> <br />
+    <input type="radio" name="role" id="engineer" value="engineer" <?php if (isset($_POST['role']) && $_POST['role'] == 'engineer')
+                                                                      echo 'checked'; ?> />
+    <br />
     <label for="manager">Manager</label>
-    <input type="radio" name="role" id="manager" value="manager" /> <br />
+    <input type="radio" name="role" id="manager" value="manager" <?php if (isset($_POST['role']) && $_POST['role'] == 'manager')
+                                                                    echo 'checked'; ?> />
+    <br />
     <label for="guest">Guest</label>
-    <input type="radio" name="role" id="guest" value="guest" checked /> <br />
+    <input type="radio" name="role" id="guest" value="guest" <?php if (isset($_POST['role']) && $_POST['role'] == 'guest')
+                                                                echo 'checked'; ?> />
     <br />
-    <br />
+    <br>
+    <br>
 
-    <label for="checkbox">Single Sign-on to the following</label> <br />
-    <input type="checkbox" name="sign[]" id="mail" value="mail" />
-    <label for="mail">Mail</label> <br />
-    <input type="checkbox" name="sign[]" id="payroll" value="payroll" />
-    <label for="payroll">Payroll</label> <br />
-    <input type="checkbox" name="sign[]" id="self" value="self" />
-    <label for="self">Self-service</label> <br />
+    <label for="checkbox">Single Sign-on to the following</label>
+    <br />
+    <input type="checkbox" name="sign[]" id="mail" value="mail" <?php if (isset($_POST['sign']) && in_array('mail', $_POST['sign']))
+                                                                  echo 'checked'; ?> />
+    <label for="mail">Mail</label>
+    <br />
+    <input type="checkbox" name="sign[]" id="payroll" value="payroll" <?php if (isset($_POST['sign']) && in_array('payroll', $_POST['sign']))
+                                                                        echo 'checked'; ?> />
+    <label for="payroll">Payroll</label>
+    <br />
+    <input type="checkbox" name="sign[]" id="self" value="self" <?php if (isset($_POST['sign']) && in_array('self', $_POST['sign']))
+                                                                  echo 'checked'; ?> />
+    <label for="self">Self-service</label>
+    <br />
 
     <input type="submit" value="Login" />
-    <input type="reset" />
+    <input type="reset" value="Reset" />
     <?php
     echo "<br>";
     echo "<br>";
     ?>
-
-    <?php
-    function test_input($data)
-    {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-      echo "Welcome, ", $_POST["username"];
-      echo "<br>";
-      echo "<br>";
-      echo "Your role is: ", $_POST["role"];
-      echo "<br>";
-      echo "<br>";
-      echo "You signed as: ";
-      if (isset($_POST["sign"])) {
-        echo implode(", ", $_POST["sign"]);
-      } else {
-        echo "There is no sign-on option selected";
-      };
-    }
-    ?>
-
-
-
-    <br />
-    <br />
   </form>
 </body>
 
---
+++ task3_11.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array²</title>
+</head>
+
+<body>
+    <?php
+    $beberages = array(
+        "Coke" => array("name" => "Coke", "price" => 2.1),
+        "Pepsi" => array("name" => "Pepsi", "price" => 2),
+        "Fanta" => array("name" => "Orange Fanta", "price" => 2.5),
+        "Trina" => array("name" => "Apple Trina", "price" => 2.3),
+    );
+
+    /*     foreach ($beberages as $name => $details) {
+        echo "Name: ", $details["name"], ". - Price: ", $details["price"];
+        echo "<br>";
+    } */
+
+    ?>
+    <form>
+        <select name="option">
+            <?php foreach ($beverages as $name => $details) : ?>
+            <option value="<?php echo $name; ?>">
+                <?php echo $details["name"], " (", $details["price"], " €)"; ?>
+            </option>
+            <?php endforeach; ?>
+        </select>
+    </form>
+</body>
+
+</html>
