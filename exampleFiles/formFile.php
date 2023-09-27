4cab9b6c4a0d811dfb684d567bd8c6b4bfd69d86
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-09-27 12:20:36 +0200

    uploadImages

---
+++ exampleFiles/formFile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FormFile</title>
+</head>
+
+<body>
+    <h2>PHP Form File Upload Example</h2>
+    <form action="fileProcess.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+</body>
+
+</html>
