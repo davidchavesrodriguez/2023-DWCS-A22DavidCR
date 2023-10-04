64a043049a7e804e2a06380459c3e0bf21a92c9e
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-10-04 11:32:21 +0200

    calculator

--- unit4/task4_1.php
+++ unit4/task4_1.php
@@ -15,9 +15,88 @@ Write a program that:
 - It creates the object firstCalcule empty. Then it assigns values the attributes using the setters. Finally it shows the values of the attributes using the getters. 
 
 - It creates the object secondCalcule assigning the values to the attributes at the moment of creation. Use the function toString to show the values of the attributes.  Shows the values returned by the multiply and add functions. -->
-<?php 
-    class Calculator {
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // Class
+    class Calculator
+    {
         private $num1;
         private $num2;
+
+        function __construct($num1 = 0, $num2 = 0)
+        {
+            $this->num1 = $num1;
+            $this->num2 = $num2;
+        }
+        /**
+         * Get the value of num1
+         */
+        public function getNum1()
+        {
+            return $this->num1;
+        }
+
+        /**
+         * Set the value of num1
+         *
+         * @return  self
+         */
+        public function setNum1($num1)
+        {
+            $this->num1 = $num1;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of num2
+         */
+        public function getNum2()
+        {
+            return $this->num2;
+        }
+
+        /**
+         * Set the value of num2
+         *
+         * @return  self
+         */
+        public function setNum2($num2)
+        {
+            $this->num2 = $num2;
+
+            return $this;
+        }
+
+        public function multiply($num1, $num2)
+        {
+            return $num1 * $num2;
+        }
+
+        public function add($num1, $num2)
+        {
+            return $num1 + $num2;
+        }
+
+        public function __toString($num1, $num2)
+        {
+            return __CLASS__;
+        }
     }
-?>
+    $firstCalcule = new Calculator();
+    $firstCalcule->setNum1(10);
+    $firstCalcule->setNum2(20);
+    echo "Values of the properties: ", $firstCalcule->getNum1(), " ", $firstCalcule->getNum2();
+    ?>
+</body>
+
+</html>
