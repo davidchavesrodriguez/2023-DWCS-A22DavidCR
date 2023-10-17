a091023374adf37adb10f2c442414e383dd3da9f
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2023-10-17 13:14:07 +0200

    array

--- unit4/task4_11/menu.php
+++ unit4/task4_11/menu.php
@@ -1,47 +1,52 @@
+<?php 
+class Menu{
+    private $type;
+    private $arrayOpcion = [];
 
-    <?php 
-    class Menu{
+    public function __construct($type){
+        $this->type = $type;
+    }
 
-        private $type;
-        private $arrayOpcion = [];
+    public function getType() {
+        return $this->type;
+    }
 
-        public function __construct($type){
-            $this->type= $type;
-        }
+    public function insertar(Opcion $opcion){
+        $this->arrayOpcion[] = $opcion;
+    }
 
-        public function getType() {
-                return $this->type;
-        }
-        public function insertar($arrayOpcion){
-
-        echo "<a href='", $arrayOpcion->getEnlace() , "'><h3 style=background-color:", $arrayOpcion->getColorFondo(), ">", $arrayOpcion->getTitulo(),  "</h3></a>";
+    public function mostrarMenu(){
+        foreach ($this->arrayOpcion as $opcion) {
+            echo "<a href='", $opcion->getEnlace() , "'><h3 style=background-color:", $opcion->getColorFondo(), ">", $opcion->getTitulo(),  "</h3></a>";
         }
     }
-    class Opcion{
-        private $titulo;
-        private $enlace;
-        private $colorFondo;
+}
 
-        public function __construct($titulo, $enlace, $colorFondo){
-            $this->titulo= $titulo;
-            $this->enlace= $enlace;
-            $this->colorFondo= $colorFondo;
-        }
+class Opcion{
+    private $titulo;
+    private $enlace;
+    private $colorFondo;
 
-        public function getTitulo() {
-                return $this->titulo;
-        }
-
-        public function getEnlace() {
-                return $this->enlace;
-        }
-
-        public function getColorFondo() {
-                return $this->colorFondo;
-        }
-
-        public function __toString()  {
-                return self::class . ": " . $this->titulo . " " . $this->enlace . " " . $this->colorFondo;
-        }   
+    public function __construct($titulo, $enlace, $colorFondo){
+        $this->titulo= $titulo;
+        $this->enlace= $enlace;
+        $this->colorFondo= $colorFondo;
     }
-    ?>
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function getEnlace() {
+        return $this->enlace;
+    }
+
+    public function getColorFondo() {
+        return $this->colorFondo;
+    }
+
+    public function __toString()  {
+        return self::class . ": " . $this->titulo . " " . $this->enlace . " " . $this->colorFondo;
+    }   
+}
+?>
