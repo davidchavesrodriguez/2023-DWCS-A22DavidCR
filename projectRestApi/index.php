e9c72794ee3501f00af8d346af061bad7975862d
A22DavidCR <A22DavidCR@iessanclemente.net>
2023-11-23 12:51:01 +0100

    Métodos

--- projectRestApi/index.php
+++ projectRestApi/index.php
@@ -9,7 +9,7 @@ $database = new Database("localhost", "gaelic", "gaelicUser", "abc123.");
 $method = new Method($database);
 
 try {
-    $players = $method->showPlayers("Turonia");
+    $players = $method->teamList();
     $json = json_encode($players, JSON_PRETTY_PRINT);
     echo $json;
 } catch (Exception $e) {
